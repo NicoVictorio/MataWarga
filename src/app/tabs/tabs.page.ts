497a49fe242bae306c78959f737ae910d2c52e24
cenz19 <vincenthadinata19@gmail.com>
2024-05-01 18:42:07 +0700

    Update Bottom Bar

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  isLoggedIn: boolean = true;
+  constructor(private router: Router) {}
 
-  constructor() { }
-
+  logout() {
+    this.isLoggedIn = false;
+    this.router.navigateByUrl('/tabs/login');
+  }
 }
