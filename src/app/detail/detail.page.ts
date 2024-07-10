60f1cc3bd55f0fbfb64ccb4d4ea05b9519ba8d95
cenz19 <vincenthadinata19@gmail.com>
2024-07-11 04:31:43 +0700

    detail sudah bisa

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { KejadianserviceService } from '../kejadianservice.service';
 import { ActivatedRoute } from '@angular/router';
-
+import { __param } from 'tslib';
 
 @Component({
   selector: 'app-detail',
@@ -9,47 +9,55 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
-  index = 0
-  kejadian: any = {}
+  index = 0;
+  kejadian: any = {};
 
-  constructor(private route: ActivatedRoute, private kejadianservice: KejadianserviceService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private kejadianservice: KejadianserviceService
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.index = params['index']
-      this.kejadianservice.kejadianDetail(params['index']).subscribe(
-        (data) => {
-          this.kejadian = data;
-        }
-      );
+    this.route.params.subscribe((params) => {
+      this.index = params['index'];
+      this.kejadianservice.kejadianDetail(params['index']).subscribe((data) => {
+        this.kejadian = data;
+      });
     });
   }
 
   like() {
-    this.kejadianservice.addLike(this.kejadian.id).subscribe(
-      (response: any) => {
+    this.kejadianservice
+      .addLike(this.kejadian.id)
+      .subscribe((response: any) => {
         if (response.result === 'success') {
           window.location.reload();
-        }
-        else {
-          alert(response.message)
+        } else {
+          alert(response.message);
         }
       });
   }
   dislike() {
-    this.kejadianservice.addDislike(this.kejadian.id).subscribe(
-      (response: any) => {
+    this.kejadianservice
+      .addDislike(this.kejadian.id)
+      .subscribe((response: any) => {
         if (response.result === 'success') {
           window.location.reload();
-        }
-        else {
-          alert(response.message)
+        } else {
+          alert(response.message);
         }
       });
   }
 
   likeComment(i: number) {
-    this.kejadianservice.addLikeComment(this.kejadian.id, i);
+    this.kejadianservice
+      .addLikeComment(this.kejadian.id, i)
+      .subscribe((response: any) => {
+        if (response.result === 'success') {
+          this.kejadian.komentar[i].jumlah_like++;
+        } else {
+          alert(response.message);
+        }
+      });
   }
-
 }
